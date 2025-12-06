45134819161582f007ab80dcbe6a427f6e0c94b8
ArtiFixal <89784139+ArtiFixal@users.noreply.github.com>
2025-12-07 00:23:17 +0100

    Fix wrong path of spring start script

--- agrest-backend/start.sh
+++ agrest-backend/start.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 CLIENT_CERTS="certs/client"
 VAULT_CERTS="certs/vault"
-SPRING_CERTS="certs/spring"
 
 export VAULT_ADDR="https://localhost:8200"
-export VAULT_CACERT="../certs/root/root-ca.crt"
+export VAULT_CACERT="certs/root/root-ca.crt"
 export VAULT_CLIENT_CERT="$CLIENT_CERTS/client.crt"
 export VAULT_CLIENT_KEY="$CLIENT_CERTS/client.key"
 export VAULT_SKIP_VERIFY="false"
-
 
 cd ..
 vault status > /dev/null
@@ -16,6 +14,7 @@ if [ $? -ne 0 ]; then
   echo "[Info]: Unsealing vault"
   bash ./unsealVaultDev.sh
 fi
+cd agrest-backend
 
 bash ./extractKeys.sh
 
@@ -24,4 +23,5 @@ java -Dserver.ssl.key-store=agrest-keystore.p12 \
   -Dserver.ssl.trust-store=agrest-truststore.p12 \
   -Dsecret.keystore.path=/dev/shm/vault/secrets/keystore \
   -Dsecret.truststore.path=/dev/shm/vault/secrets/truststore \
+  -Dapp.security.cors.allowed-origins[0]=https://localhost:5173 \
   -jar target/Agrest-0.1.jar
